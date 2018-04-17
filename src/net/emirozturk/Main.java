6659e8e1177cd47e35ce74dda319a46dd6341566
Emir Öztürk <emirozturk3@gmail.com>
2018-04-17 20:09:13 +0300

    Non-debug version
    Cleared debug values

--- src/net/emirozturk/Main.java
+++ src/net/emirozturk/Main.java
@@ -1,16 +1,13 @@
 package net.emirozturk;
 
-import com.oracle.tools.packager.Log;
-import com.sun.tools.javac.util.Convert;
-
 import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
@@ -20,16 +17,15 @@ public class Main {
         Random r = new Random();
         do
         {
-            value = BigInteger.valueOf(r.nextInt());
-        } while (value.gcd(max).equals(BigInteger.ONE));
+            value = BigInteger.valueOf(Math.abs(r.nextInt()));
+        } while (!value.gcd(max).equals(BigInteger.ONE));
         return value;
     }
 
     public static void main(String[] args) throws IOException {
 
         long start = System.currentTimeMillis();
-
-        List<String> primeStrings = Files.readAllLines(Paths.get("primes.txt"));
+        List<String> primeStrings = Files.readAllLines(Paths.get(args[0]));
         int primeCount = primeStrings.size();
 
         List<BigInteger> primeNumbers = new ArrayList<>(primeCount);
@@ -68,12 +64,6 @@ public class Main {
             eulerPhiN= eulerPhiN.multiply(eulerPhi.get(i));
         }
 
-        //DEBUGDEBUGDEBUG//
-        randome.set(0,BigInteger.valueOf(239));
-        randome.set(1,BigInteger.valueOf(151));
-        randome.set(2,BigInteger.valueOf(227));
-        randome.set(3,BigInteger.valueOf(167));
-
         int primeCounter = 0;
         for (int i = 0; i < randome.size(); i += 2)
             AValues.add(randome.get(i).modPow(randome.get(i + 1), primeNumbers.get(3 * primeCount / 2 + primeCounter++)));
@@ -90,14 +80,12 @@ public class Main {
         }
 
         BigInteger ESub = AValues.get(0);
-        //DEBUG
-        ESub = new BigInteger("1581954508210176");
-        BigInteger E = RandomWithGCD1(eulerPhi.get(0).multiply(ESub));
-        //DEBUG
-        E = BigInteger.valueOf(239);
+
+        BigInteger E = RandomWithGCD1(eulerPhiN.multiply(ESub));
+
         BigInteger D = E.modInverse(eulerPhiN.multiply(ESub));
 
-        BigInteger input = BigInteger.valueOf(786);
+        BigInteger input = BigInteger.valueOf(Integer.parseInt(args[1]));
 
         BigInteger output = input.modPow(E, primeNumbers.get(primeCount));
 
@@ -107,6 +95,16 @@ public class Main {
 
         long diff = end-start;
 
-        System.out.println(diff + " ms");
+        System.out.println("N = " + N);
+        System.out.println("Φ(N)= " + eulerPhiN);
+        for (int i = 0; i < primeCount / 2; i++)
+             System.out.println("e" + (i + 1) + "= " + randome.get(i));
+        System.out.println("E'= " + ESub);
+        System.out.println("E= " + E);
+        System.out.println("D= " + D);
+        System.out.println("input= " + input);
+        System.out.println("output= " + output);
+        System.out.println("decrypted= " + decrypted);
+        System.out.println("Süre:" + diff + " ms");
     }
 }
